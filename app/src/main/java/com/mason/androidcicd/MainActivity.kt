beff840ddb7294af4a47b538d9e9f6cdde04c4c9
mohsenMgh <mhsn.maghsoodloo@gmail.com>
2024-08-29 22:52:16 +1000

    coment@1

--- app/src/main/java/com/mason/androidcicd/MainActivity.kt
+++ app/src/main/java/com/mason/androidcicd/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidCICDTheme {
                 // A surface container using the 'background' color from the theme
+                //A this is comment
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
